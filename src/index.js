84fb5d4c20156203e76920fe531cdda10c2b53bf
mikhailsmyslov <msmyslov666@gmail.com>
2020-03-14 17:56:19 +0300

    i18n added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import '../assets/application.scss';
+import i18nInit from './i18n';
 import run from './init';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-run(window.gon);
+i18nInit().then(() => run(window.gon));
