3e5d8b26a2a81376b491958b9102464b5f308356
mikhailsmyslov <msmyslov666@gmail.com>
2020-03-15 10:22:05 +0300

    autofocus added

--- src/components/NewMessageForm.jsx
+++ src/components/NewMessageForm.jsx
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
 import { Formik, Form, Field } from 'formik';
 import { useDispatch, connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from 'react-i18next';
 import Editor from './TextEditor';
 import { actions } from '../store';
 import UserContext from '../UserContext';
 
-const TextEditor = ({ field, form, isLoading }) => {
+const TextEditor = ({
+  field, form, isLoading,
+}) => {
   const { name, value } = field;
   const { setFieldValue, submitForm } = form;
+  const { t } = useTranslation();
   const handleChange = (newValue) => setFieldValue(name, newValue);
   return (
     <Editor
@@ -15,6 +22,9 @@ const TextEditor = ({ field, form, isLoading }) => {
       onChange={handleChange}
       onCtrlEnter={submitForm}
       disabled={isLoading}
+      placeholder={t('enterMessage')}
+      className="flex-grow-1"
+      autoFocus
     />
   );
 };
@@ -36,12 +46,29 @@ const NewMessageForm = (props) => {
   return (
     <Formik initialValues={{ text: '' }} onSubmit={onSubmit}>
       {() => (
-        <Form>
+        <Form className="position-relative d-flex shadow-lg p-2">
           <Field name="text">
             {({ field, form }) => (
-              <TextEditor field={field} form={form} isLoading={isLoading} />
+              <TextEditor
+                field={field}
+                form={form}
+                isLoading={isLoading}
+              />
             )}
           </Field>
+          <small
+            className="d-none d-lg-inline position-absolute text-secondary"
+            style={{ top: '5px', right: '10px' }}
+          >
+            Отправить сообщение: Ctrl+Enter
+          </small>
+          <Button
+            type="submit"
+            className="align-self-end px-2 py-1 ml-1 btn-info"
+            disabled={isLoading}
+          >
+            <FontAwesomeIcon icon={faPaperPlane} />
+          </Button>
         </Form>
       )}
     </Formik>
--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -4,7 +4,7 @@ import { EditorState, convertToRaw, ContentState } from 'draft-js';
 import draftToHtml from 'draftjs-to-html';
 import htmlToDraft from 'html-to-draftjs';
 import classnames from 'classnames';
-import { has } from 'lodash';
+import { has, merge, isEmpty } from 'lodash';
 import Spinner from './Spinner';
 import WithWindowSize from '../hoc/withWindowSize';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
@@ -25,21 +25,28 @@ const defaultToolBarConfig = {
   emoji: { popupClassName },
 };
 
-const compactToolBarConfig = {
-  ...defaultToolBarConfig,
-  ...{ inline: { inDropdown: true } },
-  ...{ list: { inDropdown: true } },
+const compactToolBarConfig = merge(
+  {},
+  defaultToolBarConfig,
+  { inline: { inDropdown: true } },
+  { list: { inDropdown: true } },
+);
+
+const isEmptyEditorState = (editorState = {}) => {
+  if (isEmpty(editorState)) return true;
+  return !editorState.getCurrentContent().getPlainText().trim();
 };
 
-const getEditorStateFromHtml = (html) => {
-  if (!html) return EditorState.createEmpty();
+const getEditorStateFromHtml = (html = '', options = {}) => {
   const { contentBlocks, entityMap } = htmlToDraft(html);
   const contentState = ContentState.createFromBlockArray(contentBlocks, entityMap);
-  return EditorState.createWithContent(contentState);
+  const editorState = EditorState.createWithContent(contentState);
+  if (options.setFocusToEnd) return EditorState.moveFocusToEnd(editorState);
+  return editorState;
 };
 
 const getHtmlFromEditorState = (editorState) => {
-  if (!editorState) return '';
+  if (isEmptyEditorState(editorState)) return '';
   const content = editorState.getCurrentContent();
   const raw = convertToRaw(content);
   return draftToHtml(raw);
@@ -48,21 +55,27 @@ const getHtmlFromEditorState = (editorState) => {
 class Editor extends React.Component {
   static getDerivedStateFromProps = (props, state) => {
     if (!has(props, 'value')) return null;
-    const { value } = props;
+    const { value, autoFocus } = props;
     const { editorState } = state;
     const html = getHtmlFromEditorState(editorState);
     if (value === html) return null;
-    return { editorState: getEditorStateFromHtml(value) };
+    return { editorState: getEditorStateFromHtml(value, { setFocusToEnd: autoFocus }) };
   }
 
   constructor(props) {
     super(props);
-    const { value } = props;
-    this.state = { editorState: getEditorStateFromHtml(value) };
+    const { value, autoFocus } = props;
+    this.state = {
+      editorState: getEditorStateFromHtml(value, { setFocusToEnd: autoFocus }),
+    };
   }
 
   onEditorStateChange = (editorState) => {
-    this.setState({ editorState });
+    if (isEmptyEditorState(editorState)) {
+      this.setState({ editorState: EditorState.undo(editorState) });
+    } else {
+      this.setState({ editorState });
+    }
     const { onChange } = this.props;
     if (!onChange) return;
     const html = getHtmlFromEditorState(editorState);
@@ -77,12 +90,13 @@ class Editor extends React.Component {
   }
 
   render() {
-    const { className, disabled, windowSize: { deviceSize } } = this.props;
+    const {
+      className, disabled, windowSize: { deviceSize }, placeholder,
+    } = this.props;
     const { editorState } = this.state;
     const classes = classnames({
-      'h-100 position-relative p-2 shadow-lg': true,
+      'h-100 position-relative': true,
     }, className);
-
     return (
       <div className={classes}>
         {disabled && (
@@ -98,16 +112,17 @@ class Editor extends React.Component {
         </div>
         )}
         <ReactDraftWysiwyg
+          key={deviceSize}
           readOnly={disabled}
           editorState={editorState}
           onEditorStateChange={this.onEditorStateChange}
           handleReturn={this.handleReturn}
           toolbar={deviceSize === 'xs' ? compactToolBarConfig : defaultToolBarConfig}
-          editorClassName="border border-info"
-          editorStyle={{ maxHeight: '200px' }}
+          editorClassName="border border-info rounded d-flex align-items-center px-1"
+          editorStyle={{ maxHeight: '200px', minHeight: '2rem' }}
           toolbarClassName="d-flex border-0 m-0 p-0 pb-1"
           hashtag={{ separator: ' ', trigger: '#' }}
-          placeHolder="Enter message"
+          placeholder={placeholder}
         />
       </div>
     );
