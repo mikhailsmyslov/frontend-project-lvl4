dd17ff92a1ea819b6dc065d305775b614215f4cd
mikhailsmyslov <msmyslov666@gmail.com>
2020-03-16 00:44:22 +0300

    autoscroll added and refresh components on channel change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import SideBar from './SideBar';
 import Main from './Main';
 import ErrorBoundary from './ErrorBoundary';
@@ -6,22 +7,30 @@ import Header from './Header';
 import NewMessageForm from './NewMessageForm';
 import Modal from './Modal';
 
-const App = () => (
-  <ErrorBoundary>
-    <div className="vh-100 vw-100">
-      <div className="d-flex flex-column flex-md-row h-100">
-        <SideBar className="col-md-2 flex-md-column" />
-        <div className="d-flex flex-column flex-grow-1 w-100 overflow-auto">
-          <Header />
-          <Main className="h-100 overflow-auto" />
-          <NewMessageForm />
+const buildKey = (prefix, base) => [prefix, base].join('_');
+
+const App = (props) => {
+  const { currentChannelId } = props;
+  const mainKey = buildKey('main', currentChannelId);
+  const messageFormKey = ['messageForm', currentChannelId].join('_');
+  return (
+    <ErrorBoundary>
+      <div className="vh-100 vw-100">
+        <div className="d-flex flex-column flex-md-row h-100">
+          <SideBar className="col-md-2 flex-md-column" />
+          <div className="d-flex flex-column flex-grow-1 w-100 overflow-auto">
+            <Header />
+            <Main key={mainKey} className="h-100 overflow-auto" />
+            <NewMessageForm key={messageFormKey} />
+          </div>
         </div>
       </div>
-    </div>
-    <Modal />
-  </ErrorBoundary>
+      <Modal />
+    </ErrorBoundary>
+  );
+};
 
-);
+const mapStateToProps = (state) => ({ currentChannelId: state.app.currentChannelId });
 
 App.displayName = 'App';
-export default App;
+export default connect(mapStateToProps)(App);
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,32 +1,50 @@
-import React, { Fragment } from 'react';
+import React, {
+  useRef, useEffect, useState, useContext,
+} from 'react';
 import { connect } from 'react-redux';
-import { Toast } from 'react-bootstrap';
+import { Toast, Button } from 'react-bootstrap';
 import DOMPurify from 'dompurify';
 import classnames from 'classnames';
+import { debounce } from 'lodash';
 import { Trans } from 'react-i18next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import Avatar from 'react-avatar';
 import { getCurrentChannelMessages } from '../selectors';
 import UserContext from '../UserContext';
 
-const renderMessage = (message, currentUser) => {
-  const { text, author, date } = message;
+const ScrollToBottomButton = (props) => {
+  const { onClick } = props;
+  return (
+    <Button
+      variant="info"
+      className="position-sticky align-self-end d-flex justify-content-center align-items-center shadow-sm text-white"
+      style={{
+        width: '35px', height: '35px', bottom: '20px', right: '20px', opacity: '0.5', borderRadius: '50%',
+      }}
+      onClick={onClick}
+    >
+      <FontAwesomeIcon icon={faChevronDown} size="lg" />
+    </Button>
+  );
+};
+
+const Message = (props) => {
+  const { text, author, date } = props;
+  const { currentUser } = useContext(UserContext);
   const isCurrentUserMessage = author === currentUser;
-  const classes = classnames({
-    'w-75': true,
-    'my-3': true,
-    'mx-5': true,
+  const messageClasses = classnames({
+    'w-75 my-3 mx-5': true,
     'align-self-end': isCurrentUserMessage,
-    'align-self-start': isCurrentUserMessage,
+    'align-self-start': !isCurrentUserMessage,
   });
   return (
-    <div className={classes}>
+    <div className={messageClasses}>
       <Toast className="mw-100">
         <Toast.Header closeButton={false}>
           <Avatar name={author} round size={25} textSizeRatio={1} className="mr-2" />
           <strong className="mr-auto">{author}</strong>
-          <small>
-            <Trans i18nKey="formatDate">{{ date }}</Trans>
-          </small>
+          <small><Trans i18nKey="formatDate">{{ date }}</Trans></small>
         </Toast.Header>
         <Toast.Body dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(text) }} />
       </Toast>
@@ -36,19 +54,42 @@ const renderMessage = (message, currentUser) => {
 
 const Main = (props) => {
   const { className, messages } = props;
-  const classes = classnames({
-    'd-flex': true,
-    'flex-column': true,
-  }, className);
+  const mainClasses = classnames({ 'd-flex flex-column position-relative': true }, className);
+
+  const mainRef = useRef();
+  const [scrollStatus, setScrollStatus] = useState('initial');
+  const [isScrollButtonVisible, setScrollButtonVisibility] = useState(false);
+
+
+  const scrollMainToBottom = (behavior) => {
+    const main = mainRef.current;
+    main.scroll({ top: main.scrollHeight, behavior });
+  };
+
+  useEffect(() => {
+    if (scrollStatus === 'scrolling') return;
+    const behavior = scrollStatus === 'initial' ? 'auto' : 'smooth';
+    scrollMainToBottom(behavior);
+  });
+
+  const handleScroll = () => {
+    const { scrollHeight, scrollTop, clientHeight } = mainRef.current;
+    const offsetFromBottom = Math.trunc(scrollHeight - scrollTop - clientHeight);
+    const hasReachedBottom = offsetFromBottom === 0;
+    const shouldShowScrollButton = offsetFromBottom > 100;
+    setScrollButtonVisibility(shouldShowScrollButton);
+    setScrollStatus(hasReachedBottom ? 'bottom' : 'scrolling');
+  };
+
+  const debouncedHandleScroll = debounce(handleScroll, 100, { leading: true });
+
   return (
-    <UserContext.Consumer>
-      {({ currentUser }) => (
-        <main className={classes}>
-          {messages.map((message) => (
-            <Fragment key={message.id}>{renderMessage(message, currentUser)}</Fragment>))}
-        </main>
-      )}
-    </UserContext.Consumer>
+    <main ref={mainRef} className={mainClasses} onScroll={debouncedHandleScroll}>
+      {messages.map(({
+        text, author, date, id,
+      }) => <Message key={id} text={text} author={author} date={date} />)}
+      {isScrollButtonVisible && <ScrollToBottomButton onClick={() => scrollMainToBottom('smooth')} />}
+    </main>
   );
 };
 
